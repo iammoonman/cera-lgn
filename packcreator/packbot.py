8cbaae8efd27c6850056eb3bf28e93a7f5bc404d
iammoonman <420saas@gmail.com>
2022-05-21 12:20:12 -0400

    init

--- packcreator/packbot.py
+++ packcreator/packbot.py
@@ -1,25 +1,130 @@
 import json
-import interactions
+import discord
 import pickle
+import tts_output
+import io
 
 with open("token.pickle", "rb") as f:
     token = pickle.load(f)
 
-bot = interactions.Client(token=token)
+bot = discord.Bot()
 
 
 @bot.event
 async def on_ready():
     print("Logged in as")
-    # print(bot.user.name)
-    # print(bot.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print("------")
 
 
-@bot.command(name="test", description="test command")
-async def test(ctx):
-    print("hello world")
-    await ctx.send("hello world")
+set_choices = [
+    ["Fifth Edition", "5ed"],
+    ["Aether Revolt", "aer"],
+    ["Adventures in the Forgotten Realms", "afr"],
+    ["Amonkhet", "akh"],
+    ["Battlebond", "bbd"],
+    ["Battle for Zendikar", "bfz"],
+    ["Born of the Gods", "bng"],
+    ["Dragon's Maze", "dgm"],
+    ["Dark Ascension", "dka"],
+    ["Dominaria", "dom"],
+    ["Dragons of Tarkir", "dtk"],
+    ["Throne of Eldraine", "eld"],
+    ["Eternal Masters", "ema"],
+    ["Eldrich Moon", "emn"],
+    ["Fate Reforged", "frf"],
+    ["FUN", "fun"],
+    ["Guilds of Ravnica", "grn"],
+    ["Gatecrash", "gtc"],
+    ["Hour of Devastation", "hou"],
+    ["Ikoria: Lair of Behemoths", "iko"],
+    ["Journey into Nyx", "jou"],
+    ["Kaldheim", "khm"],
+    ["Kaladesh", "kld"],
+    ["Khans of Tarkir", "ktk"],
+    ["Magic 2014", "m14"],
+    ["Magic 2015", "m15"],
+    ["Core Set 2019", "m19"],
+    ["Core Set 2020", "m20"],
+    ["Core Set 2021", "m21"],
+    ["Modern Horizons 1", "mh1"],
+    ["Modern Horizons 2", "mh2"],
+    ["Midnight Hunt", "mid"],
+    ["Mirage", "mir"],
+    ["Kamigawa: Neon Dynasty", "neo"],
+    ["Oath of the Gatewatch", "ogw"],
+    ["Magic Origins", "ori"],
+    ["Rivals of Ixalan", "rix"],
+    ["Ravnica Allegaince", "rna"],
+    ["Rise of the Eldrazi", "roe"],
+    ["Return to Ravnica", "rtr"],
+    ["Shadows over Innistrad", "soi"],
+    ["Strixhaven: School of Mages", "stx"],
+    ["Theros Beyond Death", "thb"],
+    ["Theros", "ths"],
+    ["Urza's Destiny", "uds"],
+    ["Visions", "vis"],
+    ["Crimson Vow", "vow"],
+    ["War of the Spark", "war"],
+    ["Ixalan", "xln"],
+    ["Zendikar Rising", "znr"],
+]
+
+
+async def get_sets(ctx: discord.AutocompleteContext):
+    return [
+        discord.OptionChoice(s[0], s[1])
+        for s in set_choices
+        if ctx.value.lower() in s[0].lower()
+    ][:20]
+
+
+@bot.slash_command(guild_ids=[446846339427598336])
+@discord.default_permissions(manage_roles=True)
+@discord.option(
+    name="set",
+    description="Choose the set.",
+    options=[discord.OptionChoice(s[0], s[1]) for s in set_choices][:10],
+    autocomplete=get_sets,
+    type=str,
+)
+@discord.option(
+    name="num",
+    description="Number of packs.",
+    min_value=1,
+    max_value=48,
+    default=36,
+    type=int,
+)
+@discord.option(
+    name="lands", description="Include a land pack?", type=bool, default=False
+)
+async def pack(ctx: discord.ApplicationContext, set: str, num: int, lands: bool):
+    raw = tts_output.get_packs(set, num, lands)
+    packs = discord.File(
+        io.StringIO(json.dumps(raw)), filename=f"{set}_{getSeqID()}.json"
+    )
+    await ctx.respond(
+        content=f"Here are your {num} packs of {set}", file=packs, ephemeral=True
+    )
+
+
+@bot.slash_command(guild_ids=[446846339427598336])
+@discord.default_permissions(manage_roles=True)
+@discord.option(name="cc_id", description="CubeCobra ID.")
+async def cube(ctx: discord.ApplicationContext, cc_id: str):
+    await ctx.defer(ephemeral=True)
+    try:
+        raw = tts_output.get_cube(cc_id)
+    except:
+        return await ctx.respond("Something went wrong. Contact Moon.", ephemeral=True)
+    cube = discord.File(
+        io.StringIO(json.dumps(raw)), filename=f"{cc_id}_{getSeqID()}.json"
+    )
+    await ctx.respond(
+        content=f"Here is your cube with id {cc_id}.", file=cube, ephemeral=True
+    )
 
 
 def getSeqID():
@@ -35,44 +140,4 @@ def getSeqID():
     return str(e + 1)
 
 
-set_choices = []
-
-
-@bot.command(
-    name="pack",
-    description="Create packs from the set of your choice.",
-    options=[
-        interactions.Option(
-            name="set",
-            description="Should be obvious.",
-            type=interactions.OptionType.STRING,
-            required=True,
-            choices=[interactions.Choice(name=i[0], value=i[1]) for i in set_choices],
-        ),
-        interactions.Option(
-            name="number",
-            description="How many packs you want.",
-            type=interactions.OptionType.INTEGER,
-            required=True,
-            min_value=1,
-            max_value=72,
-        ),
-        interactions.Option(
-            name="lands",
-            description="Include a land pack?",
-            type=interactions.OptionType.STRING,
-            required=False,
-            choices=[
-                interactions.Choice(name="Yes", value=True),
-                interactions.Choice(name="No", value=False),
-            ],
-        ),
-    ],
-)
-async def create_pack(ctx, set: str, number: int, lands: bool):
-    # Attachments aren't implemented yet
-    # Instead of DM, use an ephemeral message in the channel.
-    return
-
-
-bot.start()
+bot.run(token)
