c8799731cf9ee179a6493540bead3b41bba777c8
iammoonman <420saas@gmail.com>
2022-06-30 12:05:29 -0400

    move cubecobra out

--- packcreator/starlight.py
+++ packcreator/starlight.py
@@ -124,22 +124,6 @@ class Starlight(commands.Cog):
         packs = discord.File(io.StringIO(json.dumps(raw)), filename=f"{set}_{self.getSeqID()}.json")
         await ctx.respond(content=f"Here are your {num} packs of {set}", file=packs, ephemeral=True)
 
-    # @commands.slash_command(guild_ids=[guild], description="Query CubeCubra for a cube.")
-    # @discord.default_permissions(manage_roles=True)
-    # @discord.option(name="cc_id", description="CubeCobra ID.")
-    # @discord.option(name="len_p", description="Size of the packs.", min=1, max=150, default=15)
-    # async def cube(self, ctx: discord.ApplicationContext, cc_id: str, len_p: int):
-    #     await ctx.defer(ephemeral=True)
-    #     try:
-    #         raw = p_caller.get_cube(cc_id, len_p)
-    #     except:
-    #         return await ctx.respond(
-    #             "Something went wrong. You may have entered an invalid CubeCobra ID. Otherwise, contact Moon.",
-    #             ephemeral=True,
-    #         )
-    #     cube = discord.File(io.StringIO(json.dumps(raw)), filename=f"{cc_id}_{self.getSeqID()}.json")
-    #     await ctx.respond(content=f"Here is your cube with id {cc_id}.", file=cube, ephemeral=True)
-
     async def get_sets_v3(ctx: discord.AutocompleteContext):
         return [discord.OptionChoice(s[0], s[1]) for s in set_choices_v3 if ctx.value.lower() in s[0].lower()][:20]
 
--- webimporter/flamewave.py
+++ webimporter/flamewave.py
@@ -1,6 +1,9 @@
+import io
+import json
 import discord
 from discord.ext import commands
 import pickle
+from webimporter import cubecobra
 
 with open("guild.pickle", "rb") as f:
     guild = pickle.load(f)
@@ -9,6 +12,22 @@ with open("guild.pickle", "rb") as f:
 class Flamewave(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.slash_command(guild_ids=[guild], description="Query CubeCubra for a cube.")
+    @discord.default_permissions(manage_roles=True)
+    @discord.option(name="cc_id", description="CubeCobra ID.")
+    @discord.option(name="len_p", description="Size of the packs.", min=1, max=150, default=15)
+    async def cube(self, ctx: discord.ApplicationContext, cc_id: str, len_p: int):
+        await ctx.defer(ephemeral=True)
+        try:
+            raw = cubecobra.get_cube(cc_id, len_p)
+        except:
+            return await ctx.respond(
+                "Something went wrong. You may have entered an invalid CubeCobra ID. Otherwise, contact Moon.",
+                ephemeral=True,
+            )
+        cube = discord.File(io.StringIO(json.dumps(raw)), filename=f"{cc_id}_{self.getSeqID()}.json")
+        await ctx.respond(content=f"Here is your cube with id {cc_id}.", file=cube, ephemeral=True)
 
 
 def setup(bot):
