31e6170c4a610212ba9f58aff68d2017f14d70c8
iammoonman <58101059+iammoonman@users.noreply.github.com>
2022-07-02 13:33:06 -0400

    Flamewave (#35)

--- packcreator/p_caller.py
+++ packcreator/p_caller.py
@@ -1,8 +1,7 @@
 import json
-import time
-import requests
-from . import tts_output, p_creator, point_slicer
-import random
+
+from exporttemplates import tts_output
+from packcreator import p_creator, point_slicer
 
 
 def get_packs(setcode, num_packs, land_pack=False):
@@ -11,18 +10,7 @@ def get_packs(setcode, num_packs, land_pack=False):
         f"setjson/{setcode}.json" if __name__ == "__main__" else f"packcreator/setjson/{setcode}.json", "rb"
     ) as f:
         setJSON = json.load(f)
-    save = {
-        "ObjectStates": [
-            {
-                "Name": "Bag",
-                "Transform": tts_output.Pack.transformAttrs,
-                "Nickname": f"{num_packs} Packs of {setcode}",
-                "ColorDiffuse": tts_output.Pack.colorAttrs,
-                "Bag": {"Order": 0},
-                "ContainedObjects": [],
-            }
-        ]
-    }
+    save = tts_output.Save(name=f"packs of {setcode}")
     all_packs = []
     for _ in range(num_packs):
         (raw_cn_cards, foil_indexes,) = p_creator.generatepack_c1c2_special(
@@ -44,7 +32,7 @@ def get_packs(setcode, num_packs, land_pack=False):
             new_colle,
             p[1],
         )
-        save["ObjectStates"][0]["ContainedObjects"].append(pack_to_add.toDict())
+        save.addObject(pack_to_add.toDict())
     if land_pack:
         basicslist = list(
             filter(
@@ -55,26 +43,15 @@ def get_packs(setcode, num_packs, land_pack=False):
         if len(basicslist) >= 5:
             pack_to_add = tts_output.Pack()
             pack_to_add.import_cards(basicslist)
-            save["ObjectStates"][0]["ContainedObjects"].append(pack_to_add.toDict())
-    return save
+            save.addObject(pack_to_add.toDict())
+    return save.getOut()
 
 
 def get_packs_v3(setcode, num_packs, land_pack=False):
     """Returns a JSON save file for Tabletop Simulator. Also returns logging information."""
     with open(f"sj3/{setcode}.json" if __name__ == "__main__" else f"packcreator/sj3/{setcode}.json", "rb") as f:
         setJSON = json.load(f)
-    save = {
-        "ObjectStates": [
-            {
-                "Name": "Bag",
-                "Transform": tts_output.Pack.transformAttrs,
-                "Nickname": f"packs of {setcode}",
-                "ColorDiffuse": tts_output.Pack.colorAttrs,
-                "Bag": {"Order": 0},
-                "ContainedObjects": [],
-            }
-        ]
-    }
+    save = tts_output.Save(name=f"packs of {setcode}")
     # log = {
     #     "seeds": [],
     #     "setcode": setcode,
@@ -114,7 +91,7 @@ def get_packs_v3(setcode, num_packs, land_pack=False):
             new_colle,
             p[1],
         )
-        save["ObjectStates"][0]["ContainedObjects"].append(pack_to_add.toDict())
+        save.addObject(pack_to_add.toDict())
     if land_pack:
         pack_to_add = tts_output.Pack()
         pack_to_add.import_cards(
@@ -126,82 +103,8 @@ def get_packs_v3(setcode, num_packs, land_pack=False):
                 )
             ]
         )
-        save["ContainedObjects"].append(pack_to_add.toDict())
-    return save  # , log
-
-
-def get_cube(cc_id, p_len):
-    """Returns a JSON save file for Tabletop Simulator."""
-    import csv
-    import copy
-
-    save = {
-        "ObjectStates": [
-            {
-                "Name": "Bag",
-                "Transform": tts_output.Pack.transformAttrs,
-                "Nickname": f"{cc_id}",
-                "ColorDiffuse": tts_output.Pack.colorAttrs,
-                "Bag": {"Order": 0},
-                "ContainedObjects": [],
-            }
-        ]
-    }
-    response = requests.get(
-        f"https://cubecobra.com/cube/download/csv/{cc_id}?primary=Color%20Category&secondary=Types-Multicolor&tertiary=Mana%20Value&quaternary=Alphabetical&showother=false",
-        headers={"UserAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"},
-    )
-    reader = csv.DictReader(response.content.decode("utf-8").splitlines())
-    if "Name" not in reader.fieldnames:  # Catching 404 errors in CubeCobra is much harder than it should be.
-        return None
-    templist = []
-    foil_indexes = []
-    for row in reader:
-        templist.append([row["Collector Number"], row["Set"]])
-        # if row["Finish"] == "Foil":
-        #     foil_indexes += [len(templist)]
-    cardinfo = tts_output.ijson_collection(templist)
-    cubelist = []
-    reader = csv.DictReader(response.content.decode("utf-8").splitlines())
-    for row in reader:
-        for c in cardinfo:
-            if row["Collector Number"] == c["collector_number"] and row["Set"] == c["set"]:
-                x = {}
-                if "card_faces" in c.keys():
-                    x = {
-                        "card_faces": [
-                            {
-                                **c["card_faces"][0],
-                                "image_uris": {
-                                    "png": row["Image URL"]
-                                    if row["Image URL"] != ""
-                                    else c["card_faces"][0]["image_uris"]["png"]
-                                },
-                            },
-                            {
-                                **c["card_faces"][1],
-                                "image_uris": {
-                                    "png": row["Image Back URL"]
-                                    if row["Image Back URL"] != ""
-                                    else c["card_faces"][1]["image_uris"]["png"]
-                                },
-                            },
-                        ],
-                        "finish": row["Finish"] == "Foil",
-                    }
-                else:
-                    x = {
-                        "image_uris": {"png": row["Image URL"] if row["Image URL"] != "" else c["image_uris"]["png"]},
-                        "finish": row["Finish"] == "Foil",
-                    }
-                cubelist.append({**c, **x})
-                break
-    random.shuffle(cubelist)
-    for i in [cubelist[u : u + p_len] for u in range(0, len(cubelist), p_len)]:
-        the_cube = tts_output.Pack()
-        the_cube.import_cards(i, [i.index(q) for q in [r for r in i if r["finish"]]])
-        save["ObjectStates"][0]["ContainedObjects"] += [the_cube.toDict()]
-    return save
+        save.addObject(pack_to_add.toDict())
+    return save.getOut()  # , log
 
 
 def get_p1p1_v3(setcode):
