d719983189f8096465072dc10773c5a6daad364d
iammoonman <420saas@gmail.com>
2022-08-07 15:25:38 -0400

    output to console

--- pagessite/src/components/sheetbuilder/filestructureform.svelte
+++ pagessite/src/components/sheetbuilder/filestructureform.svelte
@@ -6,10 +6,45 @@
 	});
 	import Distrobuilder from './distrobuilder.svelte';
 	import Slotsbuilder from './slotsbuilder.svelte';
+	import { get } from 'svelte/store';
+	import Button from '../utilities/button.svelte';
 </script>
 
 <section id="headers" class="rounded-xl grid grid-cols-1 p-1 gap-1">
 	HEADERS
+	<Button
+		text={'Print object to console'}
+		bgColorClass={'bg-green-200'}
+		on:click={() => {
+			const output2 = get(V3Store);
+			const newOutput = {
+				full_name: output2.full_name,
+				default_set: output2.default_set,
+				slots: Object.fromEntries(
+					[...output2.slots].map(([key, value]) => {
+						return [
+							key,
+							{
+								flags: value.flags,
+								options: value.options.map((o) => {
+									return { struct: Object.fromEntries([...o.struct]), freq: o.freq };
+								}),
+								sheets: Object.fromEntries([...value.sheets])
+							}
+						];
+					})
+				),
+				distros: output2.distros.map((d) => {
+					return {
+						slots: Object.fromEntries([...d.slots]),
+						drops: Object.fromEntries([...d.drops]),
+						freq: d.freq
+					};
+				})
+			};
+			console.log(JSON.stringify(newOutput));
+		}}
+	/>
 	<div class="rounded-xl flex flex-row justify-between">
 		<label for="full_name" class="p-2">Set Full Name</label>
 		<input
