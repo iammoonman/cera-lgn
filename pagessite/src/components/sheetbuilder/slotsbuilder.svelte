f9726bf22504472a15fca6454118aca4dfe50943
iammoonman <420saas@gmail.com>
2022-08-07 12:57:32 -0400

    more buttons

--- pagessite/src/components/sheetbuilder/slotsbuilder.svelte
+++ pagessite/src/components/sheetbuilder/slotsbuilder.svelte
@@ -2,6 +2,7 @@
 	import Switch from '@smui/switch';
 	import Badge from '../utilities/badge.svelte';
 	import Button from '../utilities/button.svelte';
+	import Numberinput from '../utilities/numberinput.svelte';
 	let slots: Map<
 		string,
 		{
@@ -14,77 +15,168 @@
 		}
 	> = new Map();
 	import { V3Store } from './stores';
-	let keyColors = new Map();
-	let colors: string[] = ['green', 'blue', 'red', 'orange', 'purple'];
 	V3Store.subscribe((v) => {
 		slots = v.slots;
-		keyColors = new Map(Object.keys(v.slots).map((k) => [k, colors.pop()]));
 	});
 </script>
 
 <section id="slots" class="rounded-xl p-1 grid gap-1">
 	SLOTS
-	{#each [...slots] as s}
+	{#each [...slots] as [s_key, s_value]}
 		<div class="rounded-xl p-1 grid grid-cols-1 gap-1">
 			<div class="rounded-xl p-1 flex flex-row justify-between">
-				<span>SLOT: <Badge text={s[0]} bgcolor={keyColors.get(s[0])} /></span>
-				<Button text={'Delete'} bgColorClass={'bg-red-400'} />
+				<span>SLOT: {s_key}</span>
+				<Button
+					text={'Delete'}
+					bgColorClass={'bg-red-400'}
+					on:click={() => {
+						const newSlots = slots;
+						newSlots.delete(s_key);
+						V3Store.update((oldstore) => {
+							return {
+								...oldstore,
+								slots: newSlots
+							};
+						});
+					}}
+				/>
 			</div>
 			<div class="rounded-xl p-1 flex flex-row justify-around">
 				<div class="flex justify-between items-center w-max">
 					<span class="h-min">foil</span>
 					<Switch
 						color="primary"
-						on:SMUISwitch:change={(e) =>
-							e.detail.selected
-								? s[1].flags.push('foil')
-								: (s[1].flags = s[1].flags.filter((f) => f !== 'foil'))}
+						on:SMUISwitch:change={(e) => {
+							const newFlags = e.detail.selected
+								? [...s_value.flags, 'foil']
+								: s_value.flags.filter((f) => f !== 'foil');
+							const newSlots = slots;
+							// @ts-ignore
+							newSlots.set(s_key, { ...s_value, flags: newFlags });
+							V3Store.update((oldstore) => {
+								return {
+									...oldstore,
+									slots: newSlots
+								};
+							});
+						}}
 					/>
 				</div>
 				<div class="flex justify-between items-center w-max">
 					<span class="h-min">duplicate_control</span>
 					<Switch
 						color="primary"
-						on:SMUISwitch:change={(e) =>
-							e.detail.selected
-								? s[1].flags.push('duplicate_control')
-								: (s[1].flags = s[1].flags.filter((f) => f !== 'duplicate_control'))}
+						on:SMUISwitch:change={(e) => {
+							const newFlags = e.detail.selected
+								? [...s_value.flags, 'duplicate_control']
+								: s_value.flags.filter((f) => f !== 'duplicate_control');
+							const newSlots = slots;
+							// @ts-ignore
+							newSlots.set(s_key, { ...s_value, flags: newFlags });
+							V3Store.update((oldstore) => {
+								return {
+									...oldstore,
+									slots: newSlots
+								};
+							});
+						}}
 					/>
 				</div>
 			</div>
 			<div class="grid grid-cols-[120px_minmax(100px,_1fr)] gap-1">
 				<div class="grid grid-rows-2 rounded-xl h-min mt-auto mb-auto p-1">
 					<span class="h-min text-center">OPTIONS</span>
-					<Button text={'New Option'} bgColorClass={'bg-green-200 whitespace-nowrap'} />
+					<Button
+						text={'New Option'}
+						bgColorClass={'bg-green-200 whitespace-nowrap'}
+						on:click={() => {
+							const newOptions = { struct: new Map(), freq: 1 };
+							const newSlot = s_value;
+							newSlot.options.push(newOptions);
+							const newSlots = slots;
+							newSlots.set(s_key, newSlot);
+							V3Store.update((oldstore) => {
+								return {
+									...oldstore,
+									slots: newSlots
+								};
+							});
+						}}
+					/>
 				</div>
 				<div class="rounded-xl p-1 grid gap-1">
-					{#each s[1].options as o}
+					{#each s_value.options as o}
 						<div class="rounded-xl p-1 grid grid-cols-[120px_minmax(100px,_1fr)] gap-1">
-							<div
-								class="rounded-xl p-1 flex flex-col justify-between h-min items-center gap-1 my-auto"
-							>
-								<div class="flex flex-row justify-between w-full px-3">
+							<div class="rounded-xl p-1 grid grid-cols-1 gap-1 w-max">
+								<div class="flex flex-row justify-between w-min px-3 items-center gap-1">
 									<span class="h-min text-center">FREQ</span>
-									<input
-										type="number"
-										value={0}
-										step={0.1}
-										max={1}
-										min={0}
-										class="rounded-sm w-10"
+									<Numberinput val={o.freq} step={0.1} max={1} min={0} />
+								</div>
+								<div class="flex flex-row flex-nowrap w-full gap-1">
+									<Button
+										text={'New Slot'}
+										bgColorClass={'bg-green-200'}
+										on:click={() => {
+											const newSlots = slots;
+											const newSlot = newSlots.get(s_key);
+											if (newSlot === undefined) return;
+											const myOptions = newSlot.options.filter((o2) => o2 !== o);
+											o.struct.set('x', 1);
+											newSlot.options = [...myOptions, o];
+											newSlots.set(s_key, newSlot);
+											V3Store.update((oldstore) => {
+												return {
+													...oldstore,
+													slots: newSlots
+												};
+											});
+										}}
+									/>
+									<Button
+										text={'Delete'}
+										bgColorClass={'bg-red-400'}
+										on:click={() => {
+											const newSlot = s_value;
+											newSlot.options = newSlot.options.filter(o2 => o2 != o);
+											const newSlots = slots;
+											newSlots.set(s_key, newSlot);
+											V3Store.update((oldstore) => {
+												return {
+													...oldstore,
+													slots: newSlots
+												};
+											});
+										}}
 									/>
 								</div>
-								<Button text={'New Slot'} bgColorClass={'bg-green-200'} />
 							</div>
 							<div class="grid grid-cols-1 gap-1 h-min">
-								{#each [...o.struct] as st}
+								{#each [...o.struct] as [st, st_v]}
 									<div class="rounded-xl p-1 flex justify-around items-center">
-										<span class="h-min text-center">KEY: {st[0]}</span>
+										<span class="h-min text-center">KEY: {st}</span>
 										<span class="h-min text-center">
 											NUM:
-											<input type="number" value={1} step={1} min={1} class="rounded-sm w-10" />
+											<Numberinput val={st_v} step={1} min={1} />
 										</span>
-										<Button text={'Delete'} bgColorClass={'bg-red-400'} />
+										<Button
+											text={'Delete'}
+											bgColorClass={'bg-red-400'}
+											on:click={() => {
+												const newSlots = slots;
+												const newSlot = newSlots.get(s_key);
+												if (newSlot === undefined) return;
+												const myOptions = newSlot.options.filter((o2) => o2 !== o);
+												o.struct.delete(st);
+												newSlot.options = [...myOptions, o];
+												newSlots.set(s_key, newSlot);
+												V3Store.update((oldstore) => {
+													return {
+														...oldstore,
+														slots: newSlots
+													};
+												});
+											}}
+										/>
 									</div>
 								{/each}
 							</div>
@@ -98,10 +190,10 @@
 					<Button text={'New Sheet'} bgColorClass={'bg-green-200'} />
 				</div>
 				<div class="rounded-xl p-1 grid gap-1">
-					{#each [...s[1].sheets] as sh}
+					{#each [...s_value.sheets] as [sh, sh_v]}
 						<div class="rounded-xl flex flex-row justify-between p-1 items-center gap-1">
-							<span class="h-min text-center mr-auto">KEY: {sh[0]}</span>
-							<Button text={'View'} bgColorClass={'bg-blue-400'} />
+							<span class="h-min text-center mr-auto">KEY: {sh}</span>
+							<Button text={'Edit'} bgColorClass={'bg-blue-400'} />
 							<Button text={'Delete'} bgColorClass={'bg-red-400'} />
 						</div>
 					{/each}
