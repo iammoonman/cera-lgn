94a1d46fe090093eb6d12a4e8795c075bbee3431
iammoonman <420saas@gmail.com>
2022-09-03 09:16:48 -0400

    hotfix cubecobra

--- webimporter/flamewave.py
+++ webimporter/flamewave.py
@@ -15,19 +15,31 @@ class Flamewave(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def getSeqID(self):
+        try:
+            with open("counter.pickle", "rb") as f:
+                e = pickle.load(f)
+        except:
+            with open("counter.pickle", "wb") as f:
+                pickle.dump(0, f)
+            e = 0
+        with open("counter.pickle", "wb") as f:
+            pickle.dump(e + 1, f)
+        return str(e + 1)
+
     @commands.slash_command(guild_ids=[guild], description="Query CubeCubra for a cube.")
     @discord.default_permissions(manage_roles=True)
     @discord.option(name="cc_id", description="CubeCobra ID.")
     @discord.option(name="len_p", description="Size of the packs.", min=1, max=150, default=15)
     async def cc_cube(self, ctx: discord.ApplicationContext, cc_id: str, len_p: int):
         await ctx.defer(ephemeral=True)
-        try:
-            raw = cubecobra.get_cube(cc_id, len_p)
-        except:
-            return await ctx.respond(
-                "Something went wrong. You may have entered an invalid CubeCobra ID. Otherwise, contact Moon.",
-                ephemeral=True,
-            )
+        # try:
+        raw = cubecobra.get_cube(cc_id, len_p)
+        # except:
+        #     return await ctx.respond(
+        #         "Something went wrong. You may have entered an invalid CubeCobra ID. Otherwise, contact Moon.",
+        #         ephemeral=True,
+        #     )
         cube = discord.File(io.StringIO(json.dumps(raw)), filename=f"{cc_id}_{self.getSeqID()}.json")
         await ctx.respond(content=f"Here is your cube with id {cc_id}.", file=cube, ephemeral=True)
 
