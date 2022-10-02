64e104b2453c8ef898b8d1319533df565b8ae2ec
iammoonman <420saas@gmail.com>
2022-10-02 17:35:49 -0400

    ptm symbol

--- pagessite/src/components/symbols/symbol.svelte
+++ pagessite/src/components/symbols/symbol.svelte
@@ -3,6 +3,7 @@
 	import Bluemana from './bluemana.svelte';
 	import Diamondmana from './diamondmana.svelte';
 	import Dps from './dps.svelte';
+	import Ptm from './ptm_symbol.svelte'
 	import Fourmana from './fourmana.svelte';
 	import Greenmana from './greenmana.svelte';
 	import Onemana from './onemana.svelte';
@@ -19,6 +20,9 @@
 <div style={`--size: ${symbol_size}px`}>
 	{#if symbol_name === 'dps'}
 		<Dps />
+	{/if}
+	{#if symbol_name === 'ptm'}
+		<Ptm />
 	{/if}
 	{#if symbol_name === 'W'}
 		<Whitemana />
