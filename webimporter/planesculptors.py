3d25ef7dcbfdfe810461ea7a55f75f0978b59a04
Wander van der Wal <wandervdwal@gmail.com>
2022-10-08 16:19:56 +0200

    Support for villains the musical (#80)
    Authored-by: Wander <wander@veranto.nl>

--- webimporter/planesculptors.py
+++ webimporter/planesculptors.py
@@ -10,6 +10,7 @@ legal_sets = {
     "c_ank": "ankheret",
     "c_gsc": "ghariv-the-sacred-city",
     "c_blr": "blood-like-rivers",
+    "c_vtm": "villains-the-musical",
     "c_ksv": "karslav",
     "c_alr": "alara",
     "c_hnn": "high-noon1",
