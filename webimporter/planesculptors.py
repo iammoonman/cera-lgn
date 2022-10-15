a08a809c9249f96f1278394233fffaec35adacb0
Wander <wander@veranto.nl>
2022-10-15 17:31:31 +0200

    Using hyphens instead of underscores for the c_fmq set

--- webimporter/planesculptors.py
+++ webimporter/planesculptors.py
@@ -11,7 +11,7 @@ legal_sets = {
     "c_gsc": "ghariv-the-sacred-city",
     "c_blr": "blood-like-rivers",
     "c_vtm": "villains-the-musical",
-    "c_fmq": "the_fabled_masquerade",
+    "c_fmq": "the-fabled-masquerade",
     "c_ksv": "karslav",
     "c_alr": "alara",
     "c_hnn": "high-noon1",
