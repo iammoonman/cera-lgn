f4dc5c626084ab4d26770333d4c6aa5b240cd33d
iammoonman <420saas@gmail.com>
2022-10-30 12:54:08 -0400

    update site

---
+++ docs/_app/immutable/chunks/0-5d787202.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-4ad7bf62.js";export{t as component};
--- docs/_app/immutable/chunks/1-68dceb79.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-7072f4bc.js";export{t as component};
