07aa10af83cc3423e7d6242f6f7ca1ac742bb487
iammoonman <420saas@gmail.com>
2023-01-23 20:39:26 -0500

    working import and card distinction

--- ttpencoder/Scripts/BlockScript.js
+++ ttpencoder/Scripts/BlockScript.js
@@ -1,109 +1,61 @@
-const { globalEvents } = require('@tabletop-playground/api')
+const { globalEvents } = require("@tabletop-playground/api");
 globalEvents.onChatMessage = (sender, message) => {
-    if (message.match(/^Frostwind\sfuzzy\s/g)) {
-        // Fuzzy search Scryfall for cards.
-        const [query] = message.match(/(?<=^Frostwind\sfuzzy\s).+/) ?? [null]
-        if (query !== null) {
-            fetch(`https://api.scryfall.com/cards/named?fuzzy=${encodeURI(query)}`).then(r => {
-                return r.json()
-            }).then(v => {
-                world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', sender.getCursorPosition()).setSavedData(v.id, 'sf_id')
-            })
-        }
-    }
-    if (message.match(/^Frostwind\shttps:\/\/www\.moxfield\.com\/decks\/.+/g)) {
-        const [mox_id] = message.match(/(?<=^Frostwind\shttps:\/\/www\.moxfield\.com\/decks\/).+/) ?? [null]
-        if (mox_id !== null) {
-            makeMoxfieldDeck(mox_id, sender)
-        }
-    }
-}
+	if (message.match(/^Frostwind\sfuzzy\s/g)) {
+		// Fuzzy search Scryfall for cards.
+		const [query] = message.match(/(?<=^Frostwind\sfuzzy\s).+/) ?? [null];
+		if (query !== null) {
+			fetch(`https://api.scryfall.com/cards/named?fuzzy=${encodeURI(query)}`)
+				.then((r) => {
+					return r.json();
+				})
+				.then((v) => {
+					world.createObjectFromTemplate("31E5DB224CB620FF0B35E79BB7BB8D02", sender.getCursorPosition()).setSavedData(v.id, "sf_id");
+				});
+		}
+	}
+	if (message.match(/^Frostwind\shttps:\/\/www\.moxfield\.com\/decks\/.+/g)) {
+		const [mox_id] = message.match(/(?<=^Frostwind\shttps:\/\/www\.moxfield\.com\/decks\/).+/) ?? [null];
+		if (mox_id !== null) {
+			makeMoxfieldDeck(mox_id, sender);
+		}
+	}
+};
 
 function makeMoxfieldDeck(deck_id, player) {
-    fetch(`https://api2.moxfield.com/v2/decks/all/${deck_id}`).then(r => {
-        return r.json()
-    }).then(v => {
-        const main_deck = []
-        const side_deck = []
-        const comm_deck = []
-        const comp_deck = []
-        const attr_deck = []
-        const sign_deck = []
-        const stic_deck = []
+	fetch(`https://api2.moxfield.com/v2/decks/all/${deck_id}`)
+		.then((r) => {
+			return r.json();
+		})
+		.then((v) => {
+			const main_deck = [];
+			// const side_deck = []
+			// const comm_deck = []
+			// const comp_deck = []
+			// const attr_deck = []
+			// const sign_deck = []
+			// const stic_deck = []
 
-        const { mainboard, sideboard, commanders, companions, attractions, signatureSpells, stickers } = v
+			const { mainboard, sideboard, commanders, companions, attractions, signatureSpells, stickers } = v;
 
-        Object.entries(mainboard).forEach(([k, v]) => main_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
-        Object.entries(sideboard).forEach(([k, v]) => side_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
-        Object.entries(commanders).forEach(([k, v]) => comm_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
-        Object.entries(companions).forEach(([k, v]) => comp_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
-        Object.entries(attractions).forEach(([k, v]) => attr_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
-        Object.entries(signatureSpells).forEach(([k, v]) => sign_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
-        Object.entries(stickers).forEach(([k, v]) => stic_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
+			Object.entries(mainboard).forEach(([k, v]) => main_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)));
+			// Object.entries(sideboard).forEach(([k, v]) => side_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
+			// Object.entries(commanders).forEach(([k, v]) => comm_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
+			// Object.entries(companions).forEach(([k, v]) => comp_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
+			// Object.entries(attractions).forEach(([k, v]) => attr_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
+			// Object.entries(signatureSpells).forEach(([k, v]) => sign_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
+			// Object.entries(stickers).forEach(([k, v]) => stic_deck.push(...Array(v.quantity).fill(v.card.scryfall_id)))
 
-        const m = world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', player.getCursorPosition().add([0, 0, 0]))
-        
-        main_deck.reduce((acc, curr) => {
-            if (JSON.stringify([...acc.at(-1), curr]).length < 900) {
-                acc.at(-1).push(curr)
-            } else {
-                acc.push([curr])
-            }
-            return acc
-        }, [[]]).map((v, i) => m.setSavedData(JSON.stringify(v), `sf_id_${i}`))
-        // const s = world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', player.getCursorPosition().add([1, 0, 0]))
-        // side_deck.reduce((acc, curr) => {
-        //     if (JSON.stringify([...acc.at(-1), curr]).length < 900) {
-        //         acc.at(-1).push(curr)
-        //     } else {
-        //         acc.push([curr])
-        //     }
-        //     return acc
-        // }, [[]]).map((v, i) => s.setSavedData(JSON.stringify(v), `sf_id_${i}`))
-        // const c = world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', player.getCursorPosition().add([2, 0, 0]))
-        // comm_deck.reduce((acc, curr) => {
-        //     if (JSON.stringify([...acc.at(-1), curr]).length < 900) {
-        //         acc.at(-1).push(curr)
-        //     } else {
-        //         acc.push([curr])
-        //     }
-        //     return acc
-        // }, [[]]).map((v, i) => c.setSavedData(JSON.stringify(v), `sf_id_${i}`))
-        // const p = world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', player.getCursorPosition().add([3, 0, 0]))
-        // comp_deck.reduce((acc, curr) => {
-        //     if (JSON.stringify([...acc.at(-1), curr]).length < 900) {
-        //         acc.at(-1).push(curr)
-        //     } else {
-        //         acc.push([curr])
-        //     }
-        //     return acc
-        // }, [[]]).map((v, i) => p.setSavedData(JSON.stringify(v), `sf_id_${i}`))
-        // const a = world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', player.getCursorPosition().add([4, 0, 0]))
-        // attr_deck.reduce((acc, curr) => {
-        //     if (JSON.stringify([...acc.at(-1), curr]).length < 900) {
-        //         acc.at(-1).push(curr)
-        //     } else {
-        //         acc.push([curr])
-        //     }
-        //     return acc
-        // }, [[]]).map((v, i) => a.setSavedData(JSON.stringify(v), `sf_id_${i}`))
-        // const g = world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', player.getCursorPosition().add([5, 0, 0]))
-        // sign_deck.reduce((acc, curr) => {
-        //     if (JSON.stringify([...acc.at(-1), curr]).length < 900) {
-        //         acc.at(-1).push(curr)
-        //     } else {
-        //         acc.push([curr])
-        //     }
-        //     return acc
-        // }, [[]]).map((v, i) => g.setSavedData(JSON.stringify(v), `sf_id_${i}`))
-        // const t = world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', player.getCursorPosition().add([6, 0, 0]))
-        // stic_deck.reduce((acc, curr) => {
-        //     if (JSON.stringify([...acc.at(-1), curr]).length < 900) {
-        //         acc.at(-1).push(curr)
-        //     } else {
-        //         acc.push([curr])
-        //     }
-        //     return acc
-        // }, [[]]).map((v, i) => t.setSavedData(JSON.stringify(v), `sf_id_${i}`))
-    })
+			main_deck.map((v, i) => {
+				fetch(`https://api.scryfall.com/cards/${v}`)
+					.then((r) => r.json())
+					.then((r) => {
+						const q = world.createObjectFromTemplate("31E5DB224CB620FF0B35E79BB7BB8D02", player.getCursorPosition().add([0, 0, 0]));
+						if (["normal", "adventure", "flip", "split", "meld", "leveler", "class", "saga", "planar", "vanguard", "token", "augment", "host"].includes(r["layout"])) {
+							q.setTextureOverrideURL(r["image_uris"]["normal"]);
+						} else {
+							q.setTextureOverrideURL(r["card_faces"][0]["image_uris"]["normal"]);
+						}
+					});
+			});
+		});
 }
--- ttpencoder/Scripts/CardScript.js
+++ ttpencoder/Scripts/CardScript.js
@@ -1,59 +1,44 @@
-const { refObject, Card, GlobalScriptingEvents, world, globalEvents } = require('@tabletop-playground/api')
+const { refObject, UIElement, Vector, Text, Rotator } = require("@tabletop-playground/api");
 
-refObject.onCreated = (object) => {
-    // Running on grab from deck. Do not want.
-    if (object instanceof Card) {
-        process.nextTick(() => {7
-            // If this card will be one card, 'sf_id' will be set and a string.
-            const sf_id = refObject.getSavedData('sf_id')
-            if (sf_id !== '') {
-                console.log(refObject.getSavedData('sf_id'))
-                fetch(`https://api.scryfall.com/cards/${refObject.getSavedData('sf_id')}`).then((r) => {
-                    return r.json()
-                }).then(v => {
-                    const {
-                        id,
-                        name,
-                        layout,
-                        image_uris,
-                        mana_cost,
-                        cmc,
-                        type_line,
-                        oracle_text,
-                        power,
-                        toughness,
-                        rarity,
-                        all_parts,
-                        card_faces,
-                        keywords,
-                    } = v
-                    // Remove unused properties. Max length of savedData is 1023 chars
-                    refObject.setName(name ?? 'New Card')
-                    refObject.setDescription(makeDetailedDescription(layout, { name, mana_cost, cmc, type_line, oracle_text, power, toughness, rarity }, card_faces))
-                    // This is replacing the texture override for the whole stack.
-                    refObject.setTextureOverrideURL(image_uris['large'] ?? image_uris['normal'])
-                })
-            } else {
-                console.log(`Card with id ${refObject.getId()} does not have an sf_id.`)
-            }
-            // If this card will be a deck, loop over 'sf_id_N`` until it isn't empty.
-            if (refObject.getSavedData(`sf_id_0`) === '') return;
-            for (let count = 0; count < 5; count++) {
-                console.log(count, refObject.getSavedData(`sf_id_${count}`))
-                if (refObject.getSavedData(`sf_id_${count}`) === '') return;
-                const arr = JSON.parse(refObject.getSavedData(`sf_id_${count}`))
-                arr.map(id => {
-                    // This isn't putting the cards into the Card to make a deck.
-                    // The cards seem to have the correct sf_id, but then reset to have all the same id when they are pulled from the deck.
-                    refObject.addCards(world.createObjectFromTemplate('31E5DB224CB620FF0B35E79BB7BB8D02', [0, 0, 0]).setSavedData(id, 'sf_id'))
-                })
-            }
-        })
-    }
-}
+generateCardDetails();
+
+refObject.onCreated = () => {
+	generateCardDetails();
+};
+
+refObject.onRemoved = (t) => {
+	if (t.getStackSize() > 1) return;
+	generateCardDetails();
+};
 
 // This should look a lot like the description from TTS, if possible.
-// Not sure if there's a text coloring module for setDescription. If not, redo this.
 function makeDetailedDescription(layout, easy_face, card_faces) {
-    return ''
+	const fancyDescription = new UIElement();
+    fancyDescription.scale = 0.1
+    fancyDescription.zoomVisibility = 1
+    fancyDescription.twoSided = true;
+    fancyDescription.anchorX = 0
+	fancyDescription.position = new Vector(0, -3.5, -0.1);
+    fancyDescription.rotation = new Rotator(180, 180, 0);
+	fancyDescription.widget = new Text().setText(`BOTTOM TEXT`).setFontSize(48);
+	refObject.addUI(fancyDescription);
+}
+
+function generateCardDetails() {
+	process.nextTick(() => {
+		// If this card will be one card, 'sf_id' will be set and a string.
+		const sf_id = refObject.getCardDetails()?.textureOverrideURL?.match(/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/g);
+		if (sf_id !== undefined) {
+			fetch(`https://api.scryfall.com/cards/${sf_id}`)
+				.then((r) => {
+					return r.json();
+				})
+				.then((v) => {
+					const { name, layout, mana_cost, cmc, type_line, oracle_text, power, toughness, rarity, card_faces } = v;
+					// Remove unused properties. Max length of savedData is 1023 chars
+					refObject.setName(name ?? "New Card");
+					makeDetailedDescription(layout, { name, mana_cost, cmc, type_line, oracle_text, power, toughness, rarity }, card_faces);
+				});
+		}
+	});
 }
