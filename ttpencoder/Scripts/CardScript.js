9cd8a7ec7de85f0584424a9146dfed1a29a98809
iammoonman <420saas@gmail.com>
2023-01-25 16:52:12 -0500

    swtich to refCard

--- ttpencoder/Scripts/CardScript.js
+++ ttpencoder/Scripts/CardScript.js
@@ -1,12 +1,12 @@
-const { refObject, UIElement, Vector, Text, Rotator } = require("@tabletop-playground/api");
+const { refCard, UIElement, Vector, Text, Rotator } = require("@tabletop-playground/api");
 
 generateCardDetails();
 
-refObject.onCreated = () => {
+refCard.onCreated = () => {
 	generateCardDetails();
 };
 
-refObject.onRemoved = (t) => {
+refCard.onRemoved = (t) => {
 	if (t.getStackSize() > 1) return;
 	generateCardDetails();
 };
@@ -20,14 +20,20 @@ function makeDetailedDescription(layout, easy_face, card_faces) {
     fancyDescription.anchorX = 0
 	fancyDescription.position = new Vector(0, -3.5, -0.1);
     fancyDescription.rotation = new Rotator(180, 180, 0);
-	fancyDescription.widget = new Text().setText(`BOTTOM TEXT`).setFontSize(48);
-	refObject.addUI(fancyDescription);
+	if ([].includes(layout)) {
+		fancyDescription.widget = new RichText().setText(`BOTTOM TEXT`).setFontSize(48);
+	} else if ([].includes(layout)) {
+		fancyDescription.widget = new RichText().setText(`BOTTOM TEXT`).setFontSize(48);
+	} else {
+		fancyDescription.widget = new RichText().setText(`BOTTOM TEXT`).setFontSize(48);
+	}
+	refCard.addUI(fancyDescription);
 }
 
 function generateCardDetails() {
 	process.nextTick(() => {
 		// If this card will be one card, 'sf_id' will be set and a string.
-		const sf_id = refObject.getCardDetails()?.textureOverrideURL?.match(/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/g);
+		const sf_id = refCard.getCardDetails()?.textureOverrideURL?.match(/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/g);
 		if (sf_id !== undefined) {
 			fetch(`https://api.scryfall.com/cards/${sf_id}`)
 				.then((r) => {
@@ -36,7 +42,7 @@ function generateCardDetails() {
 				.then((v) => {
 					const { name, layout, mana_cost, cmc, type_line, oracle_text, power, toughness, rarity, card_faces } = v;
 					// Remove unused properties. Max length of savedData is 1023 chars
-					refObject.setName(name ?? "New Card");
+					refCard.setName(name ?? "New Card");
 					makeDetailedDescription(layout, { name, mana_cost, cmc, type_line, oracle_text, power, toughness, rarity }, card_faces);
 				});
 		}
