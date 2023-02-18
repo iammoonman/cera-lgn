e634ed21c5119662611e9c7f64dac1eb37318aa9
iammoonman <420saas@gmail.com>
2023-02-18 09:41:05 -0500

    add player manually

--- drafthandler/glintwing.py
+++ drafthandler/glintwing.py
@@ -184,6 +184,25 @@ class StartingView(discord.ui.View):
                 view=new_view,
             )
         return await ctx.response.send_message(content="Interaction received.", ephemeral=True)
+    
+    @discord.ui.user_select(placeholder="ADD PLAYER", row=1)
+    async def add_player(self, select: discord.ui.Select, ctx: discord.Interaction):
+        print("ADD", self.id, "BY", ctx.user.id)
+        print(select)
+        if self.id not in self.bot.drafts.keys():
+            # await ctx.delete_original_message()
+            return
+        if self.bot.drafts[self.id][-1].host == str(ctx.user.id):
+            # self.bot.drafts[self.id][-1].add_player(
+            #     ctx.user.nick if ctx.user.nick is not None else ctx.user.name,
+            #     str(ctx.user.id),
+            # )
+            pass
+        await ctx.message.edit(
+            embeds=[self.bot.starting_em(self.bot.drafts[self.id][-1])],
+            view=self,
+        )
+        return await ctx.response.send_message(content="Interaction received.", ephemeral=True)
 
 
 class IG_View(discord.ui.View):
