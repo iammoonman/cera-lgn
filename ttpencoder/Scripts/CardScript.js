af1c7d3456e86c2ec916cdf17ced9465f4822646
iammoonman <420saas@gmail.com>
2023-05-12 20:40:24 -0400

    defense counters

--- ttpencoder/Scripts/CardScript.js
+++ ttpencoder/Scripts/CardScript.js
@@ -33,7 +33,8 @@ refObject.onRemoved = (t) => {
 	debounceCardDetails();
 };
 
-function createTokens() {
+function createTokens(button, player) {
+	let setting = button.getText() === "Tokens";
 	const selfURL = refObject.getCardDetails().textureOverrideURL;
 	const [front_face] = selfURL.match(/(?<=&front_face=)(false|true)/g) ?? ["true"];
 	const [sf_id] = selfURL.match(/[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/g) ?? [];
@@ -47,6 +48,8 @@ function createTokens() {
 				for (let index = 0; index < all_parts.length; index++) {
 					const element = all_parts[index];
 					if (element.id === sf_id) continue;
+					if (setting && element.component !== "token") continue;
+					if (!setting && !element.type_line.includes("Emblem")) continue;
 					if (main_stack === undefined) {
 						const c = await fetch(`https://api.scryfall.com/cards/${element.id}`);
 						const j = await c.json();
@@ -89,7 +92,7 @@ function transformCard() {
 }
 
 // This should look a lot like the description from TTS, if possible.
-function makeDetailedDescription(layout, easy_face, card_faces, front_face = true) {
+function makeDetailedDescription(layout, easy_face, [front, back] = [], front_face = true) {
 	// Add ui elements
 	const RightSide = new UIElement();
 	RightSide.height = 1060;
@@ -108,10 +111,10 @@ function makeDetailedDescription(layout, easy_face, card_faces, front_face = tru
 	// LeftSide.width = 170;
 	LeftSide.useWidgetSize = true;
 	LeftSide.zoomVisibility = 0;
-	LeftSide.position = new Vector(4.5, 5.0, -0.1);
+	LeftSide.position = new Vector(4.5, 3.5, -0.1);
 	LeftSide.rotation = new Rotator(180, 180, 0);
 	LeftSide.scale = 0.08;
-	LeftSide.anchorX = 0;
+	LeftSide.anchorX = 1;
 	LeftSide.anchorY = 0;
 	LeftSide.useTransparency = true;
 
@@ -127,7 +130,34 @@ function makeDetailedDescription(layout, easy_face, card_faces, front_face = tru
 		const Mana_Cost = easy_face.mana_cost ?? "NO MANA COST";
 		const Oracle_Text = easy_face.oracle_text ?? "";
 		const Canvas_Text = `${Name} :: ${Mana_Cost}\n${Type_Line} :: ${easy_face.rarity.toUpperCase()}\n${Oracle_Text.replace(/\(/, "[size=24][i](").replace(/\)/, ")[/i][/size]")}`;
+		console.log("HELLO")
 		RightBorder.setChild(new RichText().setText(Canvas_Text).setFontSize(24).setAutoWrap(true).setTextColor([0, 0, 0, 1]), 0, 0, 760, 1500);
+		if (easy_face["type_line"].includes("Planeswalker")) {
+			const LoyaltyText = new Button();
+			LoyaltyText.setFontSize(32);
+			LoyaltyText.setText(easy_face["loyalty"]);
+			LoyaltyText.onClicked = () => null; // Actually, this should just be text.
+			globalState.loyalty = parseInt(easy_face["loyalty"]);
+			// Use the global state to track loyalty
+
+			const LoyaltyContainer = new UIElement();
+			LoyaltyContainer.width = 100;
+			LoyaltyContainer.height = 100;
+			LoyaltyContainer.useWidgetSize = false;
+			LoyaltyContainer.zoomVisibility = 0;
+			LoyaltyContainer.position = new Vector(-3.5, -2.13, -0.1);
+			LoyaltyContainer.rotation = new Rotator(180, 180, 0);
+			LoyaltyContainer.scale = 0.08;
+			LoyaltyContainer.anchorX = 0;
+			LoyaltyContainer.anchorY = 0;
+			LoyaltyContainer.useTransparency = true;
+			LoyaltyContainer.widget = LoyaltyText;
+			refObject.addUI(LoyaltyContainer);
+			refObject.addCustomAction("Add 1 Loyalty", undefined, "add_one_loyalty");
+			refObject.addCustomAction("Lose 1 Loyalty", undefined, "lose_one_loyalty");
+			refObject.addCustomAction("Add 5 Loyalty", undefined, "add_five_loyalty");
+			refObject.addCustomAction("Lose 5 Loyalty", undefined, "lose_five_loyalty");
+		}
 	} else if (layout === "saga") {
 		// Show a nice chapter split ui
 		const Name = easy_face.name ?? "";
@@ -138,25 +168,25 @@ function makeDetailedDescription(layout, easy_face, card_faces, front_face = tru
 		RightBorder.setChild(new RichText().setText(Canvas_Text).setFontSize(24).setAutoWrap(true).setTextColor([0, 0, 0, 1]), 0, 0, 760, 1500);
 	} else if (layout === "transform") {
 		// UI is different based on which side is face up.
-		const Name = card_faces[0].name ?? "";
-		const Type_Line = card_faces[0].type_line ?? "";
-		const Mana_Cost = card_faces[0].mana_cost ?? "NO MANA COST";
-		const Oracle_Text = card_faces[0].oracle_text ?? "";
-		const Name_2 = card_faces[1].name ?? "";
-		const Type_Line_2 = card_faces[1].type_line ?? "";
-		const Mana_Cost_2 = card_faces[1].mana_cost ?? "NO MANA COST";
-		const Oracle_Text_2 = card_faces[1].oracle_text ?? "";
+		const Name = front.name ?? "";
+		const Type_Line = front.type_line ?? "";
+		const Mana_Cost = front.mana_cost ?? "NO MANA COST";
+		const Oracle_Text = front.oracle_text ?? "";
+		const Name_2 = back.name ?? "";
+		const Type_Line_2 = back.type_line ?? "";
+		const Mana_Cost_2 = back.mana_cost ?? "NO MANA COST";
+		const Oracle_Text_2 = back.oracle_text ?? "";
 		const Canvas_Text = `[color=${front_face ? "#000000" : "#777777"}]${Name} :: ${Mana_Cost}\n${Type_Line} :: ${easy_face.rarity.toUpperCase()}\n${Oracle_Text.replace(/\(/, "[size=24][i](").replace(/\)/, ")[/i][/size]")}[/color]\n-------------------------\n[color=${front_face ? "#444444" : "#000000"}]${Name_2} :: ${Mana_Cost_2}\n${Type_Line_2} :: ${easy_face.rarity.toUpperCase()}\n${Oracle_Text_2.replace(/\(/, "[size=24][i](").replace(/\)/, ")[/i][/size]")}[/color]`;
 		RightBorder.setChild(new RichText().setText(Canvas_Text).setFontSize(24).setAutoWrap(true).setTextColor([0, 0, 0, 1]), 0, 0, 760, 1500);
 		if (!front_face) {
-			refObject.setTextureOverrideURL(card_faces[1].image_uris.normal.concat("&front_face=false"));
-			if (card_faces[1]["type_line"].includes("Planeswalker")) {
+			refObject.setTextureOverrideURL(back.image_uris.normal.concat("&front_face=false"));
+			const use_loyalty_defense_back = back["type_line"].includes("Planeswalker") ? "pw" : back["type_line"].includes("Battle") ? "bt" : false;
+			if (use_loyalty_defense_back) {
 				const LoyaltyText = new Button();
 				LoyaltyText.setFontSize(32);
-				LoyaltyText.setText(card_faces[1]["loyalty"]);
+				LoyaltyText.setText(use_loyalty_defense_back === "pw" ? back["loyalty"] : back["defense"]);
 				LoyaltyText.onClicked = () => null; // Actually, this should just be text.
-				LoyaltyText.my_id = "loyalty";
-				globalState.loyalty = parseInt(card_faces[1]["loyalty"]);
+				globalState.loyalty = parseInt(back["loyalty"]);
 				// Use the global state to track loyalty
 
 				const LoyaltyContainer = new UIElement();
@@ -172,10 +202,10 @@ function makeDetailedDescription(layout, easy_face, card_faces, front_face = tru
 				LoyaltyContainer.useTransparency = true;
 				LoyaltyContainer.widget = LoyaltyText;
 				refObject.addUI(LoyaltyContainer);
-				refObject.addCustomAction("Add 1 Loyalty", undefined, "add_one_loyalty");
-				refObject.addCustomAction("Lose 1 Loyalty", undefined, "lose_one_loyalty");
-				refObject.addCustomAction("Add 5 Loyalty", undefined, "add_five_loyalty");
-				refObject.addCustomAction("Lose 5 Loyalty", undefined, "lose_five_loyalty");
+				refObject.addCustomAction("Add 1 " + (use_loyalty_defense_back === "pw" ? "Loyalty" : "Defense"), undefined, "add_one_loyalty");
+				refObject.addCustomAction("Lose 1 " + (use_loyalty_defense_back === "pw" ? "Loyalty" : "Defense"), undefined, "lose_one_loyalty");
+				refObject.addCustomAction("Add 5 " + (use_loyalty_defense_back === "pw" ? "Loyalty" : "Defense"), undefined, "add_five_loyalty");
+				refObject.addCustomAction("Lose 5 " + (use_loyalty_defense_back === "pw" ? "Loyalty" : "Defense"), undefined, "lose_five_loyalty");
 			} else {
 				const loyaltyElement = refObject.getUIs().find((ui) => ui.position.equals(new Vector(-3.5, -2.13, -0.1), 2));
 				if (!!loyaltyElement) {
@@ -187,14 +217,14 @@ function makeDetailedDescription(layout, easy_face, card_faces, front_face = tru
 				}
 			}
 		} else {
-			refObject.setTextureOverrideURL(card_faces[0].image_uris.normal.concat("&front_face=true"));
-			if (card_faces[0]["type_line"].includes("Planeswalker")) {
+			refObject.setTextureOverrideURL(front.image_uris.normal.concat("&front_face=true"));
+			const use_loyalty_defense_front = front["type_line"].includes("Planeswalker") ? "pw" : front["type_line"].includes("Battle") ? "bt" : false;
+			if (use_loyalty_defense_front) {
 				const LoyaltyText = new Button();
 				LoyaltyText.setFontSize(32);
-				LoyaltyText.setText(card_faces[0]["loyalty"]);
+				LoyaltyText.setText(use_loyalty_defense_front === "pw" ? front["loyalty"] : front["defense"]);
 				LoyaltyText.onClicked = () => null; // Actually, this should just be text.
-				LoyaltyText.my_id = "loyalty";
-				globalState.loyalty = parseInt(card_faces[0]["loyalty"]);
+				globalState.loyalty = parseInt(front["defense"]);
 				// Use the global state to track loyalty
 
 				const LoyaltyContainer = new UIElement();
@@ -210,10 +240,10 @@ function makeDetailedDescription(layout, easy_face, card_faces, front_face = tru
 				LoyaltyContainer.useTransparency = true;
 				LoyaltyContainer.widget = LoyaltyText;
 				refObject.addUI(LoyaltyContainer);
-				refObject.addCustomAction("Add 1 Loyalty", undefined, "add_one_loyalty");
-				refObject.addCustomAction("Lose 1 Loyalty", undefined, "lose_one_loyalty");
-				refObject.addCustomAction("Add 5 Loyalty", undefined, "add_five_loyalty");
-				refObject.addCustomAction("Lose 5 Loyalty", undefined, "lose_five_loyalty");
+				refObject.addCustomAction("Add 1 " + (use_loyalty_defense_front === "pw" ? "Loyalty" : "Defense"), undefined, "add_one_loyalty");
+				refObject.addCustomAction("Lose 1 " + (use_loyalty_defense_front === "pw" ? "Loyalty" : "Defense"), undefined, "lose_one_loyalty");
+				refObject.addCustomAction("Add 5 " + (use_loyalty_defense_front === "pw" ? "Loyalty" : "Defense"), undefined, "add_five_loyalty");
+				refObject.addCustomAction("Lose 5 " + (use_loyalty_defense_front === "pw" ? "Loyalty" : "Defense"), undefined, "lose_five_loyalty");
 			} else {
 				const loyaltyElement = refObject.getUIs().find((ui) => ui.position.equals(new Vector(-3.5, -2.13, -0.1), 2));
 				if (!!loyaltyElement) {
@@ -231,39 +261,19 @@ function makeDetailedDescription(layout, easy_face, card_faces, front_face = tru
 		LeftButton.onClicked = transformCard;
 		LeftBorder.addChild(LeftButton);
 	}
-	if (!!easy_face["all_parts"] && layout !== "token") {
+	if (!!easy_face["all_parts"] && easy_face["all_parts"].some((part) => part.component === "token")) {
 		const TokenButton = new Button();
 		TokenButton.setFontSize(24);
 		TokenButton.setText("Tokens");
 		TokenButton.onClicked = createTokens;
 		LeftBorder.addChild(TokenButton);
 	}
-	if (layout === "normal" && easy_face["type_line"].includes("Planeswalker")) {
-		const LoyaltyText = new Button();
-		LoyaltyText.setFontSize(32);
-		LoyaltyText.setText(easy_face["loyalty"]);
-		LoyaltyText.onClicked = () => null; // Actually, this should just be text.
-		LoyaltyText.my_id = "loyalty";
-		globalState.loyalty = parseInt(easy_face["loyalty"]);
-		// Use the global state to track loyalty
-
-		const LoyaltyContainer = new UIElement();
-		LoyaltyContainer.width = 100;
-		LoyaltyContainer.height = 100;
-		LoyaltyContainer.useWidgetSize = false;
-		LoyaltyContainer.zoomVisibility = 0;
-		LoyaltyContainer.position = new Vector(-3.5, -2.13, -0.1);
-		LoyaltyContainer.rotation = new Rotator(180, 180, 0);
-		LoyaltyContainer.scale = 0.08;
-		LoyaltyContainer.anchorX = 0;
-		LoyaltyContainer.anchorY = 0;
-		LoyaltyContainer.useTransparency = true;
-		LoyaltyContainer.widget = LoyaltyText;
-		refObject.addUI(LoyaltyContainer);
-		refObject.addCustomAction("Add 1 Loyalty", undefined, "add_one_loyalty");
-		refObject.addCustomAction("Lose 1 Loyalty", undefined, "lose_one_loyalty");
-		refObject.addCustomAction("Add 5 Loyalty", undefined, "add_five_loyalty");
-		refObject.addCustomAction("Lose 5 Loyalty", undefined, "lose_five_loyalty");
+	if (!!easy_face["all_parts"] && !easy_face.type_line.includes("Emblem") && easy_face["all_parts"].some((part) => part.type_line.includes("Emblem"))) {
+		const EmblemButton = new Button();
+		EmblemButton.setFontSize(24);
+		EmblemButton.setText("Emblems");
+		EmblemButton.onClicked = createTokens;
+		LeftBorder.addChild(EmblemButton);
 	}
 
 	RightSide.widget = RightBorder;
