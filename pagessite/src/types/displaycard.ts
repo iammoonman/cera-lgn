6dfc63755c7343e1fdac8370e94bbf69752e4e6a
iammoonman <420saas@gmail.com>
2023-05-22 18:22:45 -0400

    move roe out

--- pagessite/src/types/displaycard.ts
+++
@@ -1,9 +0,0 @@
-export type CardDisplayType = {
-    id: number;
-    title: string;
-    description?: string;
-    set: string;
-    cn: string;
-    uri?: string;
-    p_id: string; // The card's owner.
-}
