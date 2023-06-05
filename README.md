4c08b084666f38769885db067284b6cbaf1602b6
iammoonman <420saas@gmail.com>
2023-06-05 16:35:50 -0400

    move a command

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # cera-lgn
 
 This monorepo contains various connected projects surrounding the operation and management of the Discord server, MTG Limited on TTS.  
-
 Contributions, feature requests, and bug reports are welcome either via DM to an admin of MTG Limited on TTS on Discord or through the issues page on this repo.
 
 ## Credits
-Thanks to the [lethe.xyz](https://www.lethe.xyz/mtg/collation/index.html) collation project for the collation data. This project is not affiliated with them, though. Consider donating to their Patreon.</br>
-Thanks to Wizards of the Coast, as this project technically involves their intellectual property. As far as I am aware, I'm not violating any rules or laws with this project, and it is not affiliated with the company. Do not use this project to avoid having a Magic: The Gathering experience in paper, on MTGO, or on Arena. Frankly, this project is not an exact replica of real booster packs or the tournament rules, and the real thing is worth your money.</br>
+
+Thanks to the [lethe.xyz](https://www.lethe.xyz/mtg/collation/index.html) collation project for the collation data. This project is not affiliated with them, though. Consider donating to their Patreon.  
+Thanks to Wizards of the Coast, as this project technically involves their intellectual property. As far as I am aware, I'm not violating any rules or laws with this project, and it is not affiliated with the company. Do not use this project to avoid having a Magic: The Gathering experience in paper, on MTGO, or on Arena. Frankly, this project is not an exact replica of real booster packs or the tournament rules, and the real thing is worth your money.
--- stonewood/sw_flamewave.py
+++ stonewood/sw_flamewave.py
@@ -1,8 +1,13 @@
 import io
 import json
+import time
 import discord
 from discord.ext import commands
+from discord.ext.pages import Paginator, Page
 import pickle
+from requests.utils import quote
+
+import requests
 import flamewave
 from io import BytesIO
 
@@ -56,12 +61,29 @@ class Flamewave(commands.Cog):
 
     @commands.slash_command(guild_ids=[guild], description="Get the TTS cards out of MTGADraft.tk")
     @discord.option(name="file", description="The DraftLog file.", type=discord.Attachment)
-    async def tk_log(self, ctx: discord.ApplicationContext, file: discord.Attachment):
+    async def dm_log(self, ctx: discord.ApplicationContext, file: discord.Attachment):
         await ctx.defer(ephemeral=True)
         f = BytesIO(await file.read())
         f2, n = flamewave.draftmancer.full_draftmancer_log(f)
         new_file = discord.File(io.StringIO(json.dumps(f2)), filename=f"draft_{n}.json")
         await ctx.respond(file=new_file, ephemeral=True)
+        return
+
+    @commands.slash_command(guild_ids=[guild], description="Search Scryfall for cards.")
+    @discord.option(name="query", description="Query for cards the same way you would on Scryfall.", type=str)
+    async def scry_search(self, ctx: discord.ApplicationContext, query: str):
+        await ctx.defer()
+        full_json = []
+        full_set_json = []
+        time.sleep(0.25)
+        response = requests.get(f"https://api.scryfall.com/cards/search?q={quote(query)}", headers={"UserAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"})
+        full_json += (resjson := response.json())["data"]
+        while resjson["has_more"]:
+            time.sleep(0.25)
+            response = requests.get(resjson["next_page"], headers={"UserAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"})
+            full_set_json += (resjson := response.json())["data"]
+        paginator = Paginator(pages=[Page(content=f'{c["scryfall_uri"][:-3]}discord') for c in full_json])
+        await paginator.respond(ctx.interaction)
         return
 
 
