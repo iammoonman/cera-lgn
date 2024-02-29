b07d37afe613bf73b0c19fb90fefc5299fd40208
iammoonman <420saas@gmail.com>
2024-02-29 10:06:07 -0500

    note sure why this isnt working

--- flamewave/postCollection.js
+++ flamewave/postCollection.js
@@ -1,6 +1,6 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, BatchGetCommand } from "@aws-sdk/lib-dynamodb";
 const s3 = new S3Client({ region: "us-east-1" });
 const dbc = new DynamoDBClient({ region: "us-east-1" });
 const doc = DynamoDBDocumentClient.from(dbc);
@@ -15,37 +15,35 @@ export const handler = async (event) => {
 		return "{}"
 	}
 	if (postData.length < 2) return "{}"
-	const ids = [];
-	const quants = [];
+	let requests = [];
 	for (let { flamewave_id, scryfall_id, set, cn, oracle_id, quantity } of postData) {
-		let command = null;
 		if (flamewave_id) {
 			// Verify that the flamewave_id is valid.
-			command = new GetCommand({ TableName: "flamewave", Key: { flamewave_id }, AttributesToGet: ["flamewave_id"] });
+			requests.push({ flamewave_id })
 		} else if (scryfall_id) {
-			command = new GetCommand({ TableName: "flamewave", Key: { scryfall_id }, AttributesToGet: ["flamewave_id"] });
+			requests.push({ scryfall_id })
 		} else if (set && cn) {
-			command = new GetCommand({ TableName: "flamewave", Key: { set, cn }, AttributesToGet: ["flamewave_id"] });
+			requests.push({ set, cn })
 		} else if (oracle_id) {
-			command = new GetCommand({ TableName: "flamewave", Key: { oracle_id }, AttributesToGet: ["flamewave_id"] });
+			requests.push({ oracle_id })
 		}
-		if (command === null) {
-			return "{}"
-		}
-		try {
-			const resp = await doc.send(command);
-			if (resp.Item !== undefined) {
-				const i = ids.findIndex(v => v === resp.Item["flamewave_id"])
+	}
+	const quants = [];
+	const ids = [];
+	for (let ind = 0; ind < requests.length; ind += 25) {
+		let command = new BatchGetCommand({ RequestItems: { flamewave: { Keys: requests.slice(ind, ind + 25), AttributesToGet: ["flamewave_id"] } } })
+		let resp = await doc.send(command);
+		if (resp.Responses !== undefined) {
+			resp.Responses.flamewave.forEach((e) => {
+				let i = ids.findIndex(v => v === e["flamewave_id"])
 				if (i > -1) {
-					quants[i] += quantity
+					quants[i] += 1
 				} else {
-					ids.push(resp.Item["flamewave_id"]);
-					quants.push(quantity ?? 1);
+					ids.push(e["flamewave_id"]);
+					quants.push(1);
 				}
-			} else {
-				return "{}"
-			}
-		} catch (e) {
+			})
+		} else {
 			return "{}"
 		}
 	}
@@ -81,6 +79,10 @@ export const handler = async (event) => {
 			}
 		}
 	})
+	if (deckids.length < 2) {
+		console.log(objentries)
+		return "{}"
+	}
 	return JSON.stringify({
 		Name: "Deck",
 		Transform: {
