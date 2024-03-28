26734ea7edbed9d37b08b07fbe34aa7a42b0fb88
iammoonman <420saas@gmail.com>
2024-03-28 10:38:08 -0400

    bad command

--- flamewave/getFlamewaveCard.js
+++ flamewave/getFlamewaveCard.js
@@ -4,22 +4,17 @@ const dbc = new DynamoDBClient({ region: "us-east-1" });
 const doc = DynamoDBDocumentClient.from(dbc);
 export const handler = async (event) => {
 	const id = event.queryStringParameters?.q ?? "0000579f-7b35-4ed3-b44c-db2a538066fe";
-	try {
-		const getObjectCommand = new GetCommand({
-			TableName: "cards",
-			Key: {
-				flamewave_id: id
-			}
-		});
-		const response = await doc.send(getObjectCommand);
-		const data = await response.Body.transformToString("utf-8");
-		const out = JSON.parse(data);
-		return JSON.stringify({
-			CardID: 100,
-			CustomDeck: { "1": out.img },
-			...out.obj,
-		});
-	} catch {
-		return JSON.stringify({});
-	}
+	const getObjectCommand = new GetCommand({
+		TableName: "flamewave",
+		Key: {
+			flamewave_id: id
+		}
+	});
+	const response = await doc.send(getObjectCommand);
+	const data = response.Item;
+	return JSON.stringify({
+		CardID: 100,
+		CustomDeck: { "1": data.img },
+		...data.obj,
+	});
 };
